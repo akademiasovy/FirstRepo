f84d426134c0a815eae9ccfa34528af027c703aa
matuskokoska <matus.kokoska@akademiasovy.sk>
2015-11-13 12:41:13 +0100

    main C

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    printf("\n\t***************\n");
+    printf("\t*Matus Kokoska*\n");
+    printf("\t***2015/2016***\n");
+    printf("\t***************\n");
+    printf("\n\n\n");
+    int a, b=752, c; // a=1546
+    c=a+b;
+    printf("hodnota premennej c je: %d\n",c);
+    printf("%d+%d=%d\n",a,b,c);
+    float x=26.63546, y=1.548454,z;
+    z=x*y;
+    printf("%f * %f = %f\n",x,y,z);  
+  system("PAUSE");	
+  return 0;
+}
